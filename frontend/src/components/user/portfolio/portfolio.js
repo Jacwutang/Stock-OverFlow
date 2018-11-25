c7ecbf12faf715200912a0f912d24be82092f428
Jacwutang <jackwu350@gmail.com>
2018-11-24 20:37:11 -0800

    create users portfolio component

---
+++ frontend/src/components/user/portfolio/portfolio.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Portfolio extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <h1> Portfolio </h1>
+    );
+  }
+}
+
+export default Portfolio;
