339e89ad45abc84cf890edfa42f7ebecbad631bc
Jacwutang <jackwu350@gmail.com>
2018-11-24 18:00:30 -0800

    add links to header nav

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,26 +1,30 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import Search from "../search";
 import styles from "./index.module.css";
 import Logo from "assets/images/logo.svg";
+
 const Header = (props) => {
 
-  const renderList = () => (
-    props.loggedIn?
-    ( <ul className={styles.navbar_right}>
-        <li> Simulator </li>
-        <li onClick={props.logout}> Log out </li>
-      </ul>
-    )
-    :
-    ( <ul className={styles.navbar_right}>
-        <li> Sign Up </li>
-        <li> Log In </li>
-        <li> Simulator </li>
-      </ul>
-    )
-  );
-
-
+  const renderList = () => {
+    if(props.loggedIn) {
+      return(
+        <ul className={styles.navbar_right}>
+            <li> <Link to="/simulator"> Simulator </Link> </li>
+            <li onClick={props.logout}> Log out </li>
+          </ul>
+      );
+    }
+    else {
+      return(
+        <ul className={styles.navbar_right}>
+           <li> <Link to="/signup"> Sign Up </Link> </li>
+           <li> <Link to="/login"> Log In </Link>  </li>
+           <li> <Link to="/simulator"> Simulator </Link>  </li>
+         </ul>
+      );
+    }
+  }
   return (
     <nav className={styles.nav}>
       <div className={styles.navbar_left}>
